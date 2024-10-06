5d307f27c4248e18eaa30ead0e92f595fc0ebbf8
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-06 21:13:42 +0600

    dynamic data grap from user input

--- src/component/UseReduserFun.jsx
+++ src/component/UseReduserFun.jsx
@@ -10,20 +10,30 @@ const reduser = (currentState, action) => {
 
     switch (action.type1) {
         case 'addName':
-            return { ...currentState, name: 'shakil'}
+            return { ...currentState, name: action.payload }
+        case 'addAge':
+            return { ...currentState, age: action.payload }
+        case 'addHobies':
+            return { ...currentState, hobbies:[...currentState.hobbies,action.payload]  }
+        default:
+            return currentState;
     }
 }
 
 const UseReduserFun = () => {
 
-
     const [state, dispatch] = useReducer(reduser, initialState);
     console.log(state);
+
+    const handSubmit = (e) => {
+        e.preventDefault()
+        console.log(state);
+    }
     return (
-        <form>
+        <form onClick={handSubmit}>
             
             <input
-                onChange={() => dispatch({type1: 'addName'})}
+                onChange={(e) => dispatch({type1: 'addName', payload: e.target.value})}
                 className="p-2 border ml-20 mt-20 rounded "
                 type="text"
                 name="name"
@@ -33,11 +43,20 @@ const UseReduserFun = () => {
             </input>
             
             <input
+                 onChange={(e) => dispatch({type1: 'addAge', payload: e.target.value})}
                 className="p-2 border ml-20 mt-20 rounded "
                 type="number"
                 name="age"
                 placeholder="age"
             ></input>
+            <input
+                 onBlur={(e) => dispatch({type1: 'addHobies', payload: e.target.value})}
+                className="p-2 border ml-20 mt-20 rounded "
+                type="hobbies"
+                name="hobbies"
+                placeholder="hobbies"
+            ></input>
+            <button className="text-white border p-2 ml-10 rounded bg-slate-400">Submit</button>
         </form>
     );
 };
